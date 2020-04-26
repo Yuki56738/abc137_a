3fb96123ee4191ee88f96c5dc7f0a451eea4661b
Yuki <you@example.com>
2020-04-26 17:00:51 +0900

    Second commit

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 # https://atcoder.jp/contests/abc137/tasks/abc137_a
-import sys
-str=input()
-str2=str.split()
-A=int(str2[0])
-B=int((str2[1]))
-AplusB=A+B
-AminusB=A-B
-AxB=A*B
-print(max(AplusB,AminusB,AxB))
+str=input().split()
+A=int(str[0])
+B=int(str[1])
+print(max(A+B,A-B,A*B))
